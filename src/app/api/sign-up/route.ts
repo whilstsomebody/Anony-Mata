1122748968fba0e20cd0cf6659b46edec25aaba1
Aman Vishwakarma <vishwakarmarambhawan576@gmail.com>
2024-07-06 20:52:02 +0530

    Signup user method

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,108 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import bcryptjs from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+        const { username, email, password } = await request.json();
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Username already taken"
+                },
+                {
+                    status: 400
+                }
+            )
+        }
+
+        const existingUserByEmail = await UserModel.findOne({ email })
+
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        if (existingUserByEmail) {
+            if (existingUserByEmail.isVerified) {
+                return Response.json(
+                    {
+                        success: false,
+                        message: "Email already registered"
+                    },
+                    {
+                        status: 400
+                    }
+                )
+            } else {
+                const hashedPassword = await bcryptjs.hash(password, 10)
+                existingUserByEmail.password = hashedPassword
+                existingUserByEmail.verifyCode = verifyCode
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await existingUserByEmail.save()
+            }
+        } else {
+            const hashedPassword = await bcryptjs.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messageS: []
+            })
+
+            await newUser.save()
+        }
+
+        // send verification email
+        const emailResponse = await sendVerificationEmail(email, username, verifyCode)
+
+        console.log("Email response: ", emailResponse)
+
+        if (emailResponse.success) {
+            return Response.json(
+                {
+                    success: true,
+                    message: "User registered successfully. Please verify your email address."
+                },
+                {
+                    status: 201
+                }
+            )
+        } else {
+            return Response.json(
+                {
+                    success: false,
+                    message: emailResponse.message
+                },
+                {
+                    status: 500
+                }
+            )
+        }
+    } catch (error) {
+        console.log("Error while signing up: ", error)
+
+        return Response.json(
+            {
+                success: false,
+                message: "Error while signing up user"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
