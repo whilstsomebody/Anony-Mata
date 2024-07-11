747f6c1271490730fc2ac016cff9bb39dfdc4845
Aman Vishwakarma <vishwakarmarambhawan576@gmail.com>
2024-07-11 13:05:12 +0530

    added dashboard, message card

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,6 +3,6 @@ import { Message } from "@/model/User.model";
 export interface ApiResponse {
     success: boolean;
     message: string;
-    isAcceptingMessages?: boolean;
+    isAcceptingMessage?: boolean;
     messages?: Array<Message>;
 }
